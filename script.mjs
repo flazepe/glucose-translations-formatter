a5bea705525b88433b638e91fa8787383191834e
flazepe <51487578+flazepe@users.noreply.github.com>
2024-03-29 17:13:06 +0800

    refactor: remove unneeded code

--- script.mjs
+++ script.mjs
@@ -2,7 +2,7 @@ import { format } from "https://esm.run/prettier";
 import htmlPlugin from "https://esm.run/prettier/plugins/html.mjs";
 
 export async function formatChapter(file) {
-	log(`Formatting chapter file: ${file.name}`);
+	log(`Formatting ${file.name}`);
 
 	let text = `<?xml version="1.0" encoding="utf-8"?>
 		<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
